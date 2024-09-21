e60d2268d2f2799ea90a7f9053d05a063f8eff81
97-naushad <sonu.naushad@gmail.com>
2024-09-21 19:55:09 +0300

    Testing webhook trigger (#8)
    * Changed project from terragrunt to terraform
    
    * testing webhook trigger
    
    * updated default variables
    
    ---------
    
    Co-authored-by: Naushad Ahmad <opsnaushad@gmail.com>

---
+++ monitoring.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+  namespace  = kubernetes_namespace_v1.monitoring.id
+}
+
+resource "kubernetes_namespace_v1" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
