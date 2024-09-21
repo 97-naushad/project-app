90fe9de4125172b93fbef95a7109fb516044d5e2
Naushad Ahmad <opsnaushad@gmail.com>
2024-09-21 14:06:55 +0300

    Deploy Atlantis using Terraform and Helm

---
+++ modules/project-app/main.tf
@@ -0,0 +1,20 @@
+resource "helm_release" "atlantis" {
+  name       = var.app_name
+  chart      = "atlantis"
+  repository = "https://runatlantis.github.io/helm-charts"
+  namespace  = kubernetes_namespace_v1.namespace.id
+
+  values = [
+    templatefile("${path.module}/templates/values.yaml", {
+      gh_user    = var.gh_user
+      gh_token   = var.gh_token
+      git_secret = var.git_secret
+    })
+  ]
+}
+
+resource "kubernetes_namespace_v1" "namespace" {
+  metadata {
+    name = var.namespace
+  }
+}
